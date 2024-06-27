d7a648c57dc0d2c7324cd6069aef5703d71fcb90
mayorwise001 <tomosorijoseph@yahoo.com>
2024-06-27 04:47:07 +0100

    ok

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express")
-const indexRouter = require("./routes/index")
-const router = express.Router();
+
+const bcrypt = require("bcrypt");
 const path = require("path");
+const session = require("express-session");
+const flash = require('connect-flash');
+const passport = require("passport"); 
+const indexRouter = require('./routes/index')
+const http = require('http');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const debug = require('debug')('my-express-app:server');
+
+const methodOverride = require('method-override');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
